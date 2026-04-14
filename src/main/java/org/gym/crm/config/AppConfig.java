080a642323b680cab62d5ee8d13e2d60b1468703
Denys <deniska2002kulikov@gmail.com>
2026-04-14 13:35:16 +0300

    added context, edited project name with pom

---
+++ src/main/java/org/gym/crm/config/AppConfig.java
@@ -0,0 +1,11 @@
+package org.gym.crm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = "com.gym.crm")
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+}
